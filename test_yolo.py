ba61cd451ddf3d64b768a065d09c34b99ecdb2c8
Filippo <filippofedericoni@hotmail.com>
2025-05-28 09:28:37 +0200

    new yolo mdel

--- test_yolo.py
+++ test_yolo.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO('./runs_90_epochs/segment/train/weights/best.pt')
+    model = YOLO('./runs/segment/train/weights/best.pt')
     
     # model.predict('./Dataset/dataset/images/test',save=True)
-    model.predict('./Dataset/LC 5 sec clips 30fps/Utswset-Case 8-007.mp4',show=True)
+    model.predict('./Dataset/LC 5 sec clips 30fps/Adnanset-Lc 10-003.mp4',show=True)
