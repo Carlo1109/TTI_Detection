7bb3c680db74f1a48ba56da874e081640b21195f
Filippo <filippofedericoni@hotmail.com>
2025-05-29 16:41:41 +0200

    depth anything

--- test_yolo.py
+++ test_yolo.py
@@ -1,13 +1,25 @@
 from ultralytics import YOLO
 
+
+from transformers import pipeline
+from PIL import Image 
+
 if __name__ == "__main__":
-    model = YOLO('./runs/segment/train/weights/best.pt')
+   
+
+    pipe = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Small-hf")
+    image = Image.open('./Dataset/dataset/images/test/video0008_frame0035.png')
+    depth = pipe(image)["depth"]
+    depth.show()
+    # Image.show(depth)
+  
+    # model = YOLO('./runs/segment/train/weights/best.pt')
     
-    res = model.predict('./Dataset/dataset/images/test/video0014_frame0011.png')
+    # res = model.predict('./Dataset/dataset/images/test/video0014_frame0011.png')
     # model.predict('./Dataset/LC 5 sec clips 30fps/Adnanset-Lc 10-003.mp4',show=True)
-    print(len(res[0]))
-    for r in res:
-        pass
+    # print(len(res[0]))
+    # for r in res:
+    #     pass
         # print(len(r.masks.data))
         # print(r.boxes.cls)
         # print(r.masks.data)     
