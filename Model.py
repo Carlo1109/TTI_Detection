c4dcebd427127369c5385115cfa96a1018557c43
Filippo <filippofedericoni@hotmail.com>
2025-06-25 18:37:58 +0200

    evalutaion

--- Model.py
+++ Model.py
@@ -22,3 +22,24 @@ class ROIClassifier(nn.Module):
         features = self.backbone(x)   
         out = F.sigmoid(self.fc(features))
         return out
+    
+    
+
+class ROIClassifierNoDepth(nn.Module):
+    def __init__(self, num_hoi_classes):
+        super().__init__()
+      
+        self.pre_conv = nn.Conv2d(4, 3, kernel_size=1, stride=1, padding=0, bias=False)
+        
+        self.backbone = models.resnet18(pretrained=True)
+        self.backbone.fc = nn.Identity()
+        
+        self.fc = nn.Linear(512, num_hoi_classes)
+        
+    def forward(self, x):
+        
+        x = self.pre_conv(x)         
+        features = self.backbone(x)   
+        out = F.sigmoid(self.fc(features))
+        return out
+
