93f26959ddfc0af140300d2331ecace26722ea21
Filippo <filippofedericoni@hotmail.com>
2025-07-18 17:54:25 +0200

    dataset for dino

--- Filippo/Test/create_full_dataset.py
+++ Filippo/Test/create_full_dataset.py
@@ -2,15 +2,42 @@ import cv2
 import os
 
 
-
-
 VIDEO_FOLDER = '../../Dataset/video_dataset/videos/train/'
-OUTPUT_FOLDER = 
+OUTPUT_FOLDER = '../Full Dataset/train/'
+
+
+def _load_video(video_path):
+    # Load video
+    cap = cv2.VideoCapture(video_path)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    return cap, frame_count
+
+
 
 def create_datatset():
-    pass
+    
+    videos = os.listdir(VIDEO_FOLDER)
+    i = 1
+    
+    for video in videos:
+        print(f'Processing Video {i}/{len(videos)}')
+        
+        cap, frame_count = _load_video(os.path.join(VIDEO_FOLDER, video))
+        
+        count = 0
+        success = True
+        while success:
+            success,image = cap.read()
 
+            if count%8 == 0 and count < frame_count :
+                print(f"Frame {count}/{frame_count} ----> {video}")
+                cv2.imwrite(OUTPUT_FOLDER + f'video{i:04d}_frame%d.jpg'%count,image)
+            count+=1
+        i+=1
+       
+
+    
 
 
 if __name__ == "__main__":
-    pass
+    create_datatset()
