34f3fd02c037fb5b0fa375b17a4204362a477b12
Filippo <filippofedericoni@hotmail.com>
2025-07-18 16:22:19 +0200

    Create create_full_dataset.py

---
+++ Filippo/Test/create_full_dataset.py
@@ -0,0 +1,16 @@
+import cv2
+import os
+
+
+
+
+VIDEO_FOLDER = '../../Dataset/video_dataset/videos/train/'
+OUTPUT_FOLDER = 
+
+def create_datatset():
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
