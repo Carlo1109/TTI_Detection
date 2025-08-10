208df6874cba3098850bb8c0a6f1b9b98b62e849
Filippo <filippofedericoni@hotmail.com>
2025-08-10 12:48:55 +0200

    working masks

--- Filippo/Test/test_mask_rcnn.py
+++ Filippo/Test/test_mask_rcnn.py
@@ -38,7 +38,7 @@ def get_maskrcnn(num_classes=3):
 
     return model
 
-def vis(model,im,score_thresh=0.1, mask_thresh=0.1):
+def vis(model,im,score_thresh=0.4, mask_thresh=0.4):
     img = cv2.imread(im)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
     
@@ -51,29 +51,42 @@ def vis(model,im,score_thresh=0.1, mask_thresh=0.1):
     model.eval()
     with torch.no_grad():
         preds = model([inp])[0]
-    print(preds)
+    # print(preds)
     # masks  = preds["masks"].squeeze(1).cpu().numpy()      # (N, H, W)
     # labels = preds["labels"].cpu().numpy().astype(int)    # (N,)
     # scores = preds["scores"].cpu().numpy()                # (N,)
     
-    for mask, label, score in zip(preds["masks"], preds["labels"], preds["scores"]):
+    H,W,_ = img.shape
+    scale_x = W / IMG_SIZE[0]
+    scale_y = H / IMG_SIZE[1]
+    for mask, label, score, box in zip(preds["masks"], preds["labels"], preds["scores"], preds["boxes"]):
         score = score.cpu().numpy()   
-        label = label.cpu().numpy()   
+        label = label.cpu().numpy()
+        box = box.cpu().numpy()   
         if score < score_thresh:
             continue
+        
         mask_bin = mask.squeeze(0).cpu().numpy() >= mask_thresh
-        plt.imshow(mask_bin)
+        mask_bin = cv2.resize(mask_bin.astype(np.uint8),(W,H))
+        mask_bin = np.ma.masked_where(mask_bin == 0, mask_bin)        
+        plt.imshow(img)
+        plt.imshow(mask_bin,alpha=0.5, cmap='jet')
+        x1, y1, x2, y2 = box
+        x1 *= scale_x ; x2 *= scale_x ;y1 *= scale_y ; y2 *= scale_y
+        rect = plt.Rectangle((x1, y1), x2 - x1, y2 - y1,
+                            fill=False, color='green', linewidth=1.5)
+        plt.gca().add_patch(rect)
+
         plt.title(f"Class {label.item()} | Score {score:.2f}")
         plt.axis("off")
         plt.show()
 
 
 if __name__ == '__main__':
-    # model = get_maskrcnn()
-    model = torchvision.models.detection.maskrcnn_resnet50_fpn_v2(weights="DEFAULT")
+    model = get_maskrcnn()
     model.to(DEVICE)
     img_fp = "../../Dataset/evaluation/images/video0001_frame0000.png"
-    # sd = torch.load(CHECKPOINT, map_location=DEVICE)
-    # missing, unexpected = model.load_state_dict(sd, strict=False)
+    sd = torch.load(CHECKPOINT, map_location=DEVICE)
+    missing, unexpected = model.load_state_dict(sd, strict=False)
 
     vis(model,img_fp)
