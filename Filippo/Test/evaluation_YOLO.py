5b0f9cd8637e3c471b2ab832922f00dba660b821
Filippo <filippofedericoni@hotmail.com>
2025-09-06 18:20:20 +0200

    Update evaluation_YOLO.py

--- Filippo/Test/evaluation_YOLO.py
+++ Filippo/Test/evaluation_YOLO.py
@@ -8,8 +8,7 @@ import random
 from ultralytics import YOLO
 from utils import _load_video, _load_frame, normalize , get_polygon ,  parse_mask_string ,to_tool_id , to_tti_id , points_to_mask
 import json
-
-
+from sklearn.metrics import accuracy_score
 
 IMG_SIZE    = (256, 256)
 DEVICE      = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -199,8 +198,14 @@ def expand_mask(mask, pixels):
 def vis(model,im,depth_model):
 
     preds = model.predict(im,verbose=False)
+    
+    ann = preds[0].plot() 
+    ann_rgb = cv2.cvtColor(ann, cv2.COLOR_BGR2RGB) 
+    plt.imshow(ann_rgb)
+    plt.show()
+    
     img = cv2.imread(im)
-    print(img.shape)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     
     tool_list = []
     tissue_list = []
@@ -217,7 +222,7 @@ def vis(model,im,depth_model):
         
         for i in range(masks.shape[0]):
             mask_resized = cv2.resize(masks[i].cpu().numpy().astype('uint8'), (img.shape[1], img.shape[0]), interpolation=cv2.INTER_NEAREST)
-            print(mask_resized.shape)
+            # print(mask_resized.shape)
             if clss[i] == 1:
                 tool_list.append(mask_resized)
             else:
@@ -226,7 +231,7 @@ def vis(model,im,depth_model):
            
     pairs = []
     for tool in tool_list:
-        print("tool found")
+        # print("tool found")
         tool_mask = tool
         depth_map = np.array(depth_model(im)["depth"]) / 255
         tool_depth = depth_map[tool_mask.astype(bool)]
@@ -249,15 +254,15 @@ def vis(model,im,depth_model):
             inter_numb = intersection_mask.sum()
             
             plt.imshow(img)
-            plt.imshow(expand_mask(np.ma.masked_where(tool_mask == 0, tool_mask)  ,10),alpha=0.7)
-            plt.imshow(expand_mask(np.ma.masked_where(tissue_mask == 0, tissue_mask),10),alpha=0.7)
+            plt.imshow(np.ma.masked_where(expand_mask(tool_mask,10) == 0,expand_mask(tool_mask,10)),alpha=0.7)
+            plt.imshow(np.ma.masked_where(expand_mask(tissue_mask,10) == 0,expand_mask(tissue_mask,10)),alpha=0.7)
             plt.show()
             
-            if inter_numb > 15:
-                plt.imshow(img)
-                plt.imshow(np.ma.masked_where(intersection_mask == 0, intersection_mask)  ,alpha=0.8)
-                plt.title("PRE EXPAND")
-                plt.show()
+            if inter_numb > 5:
+                # plt.imshow(img)
+                # plt.imshow(np.ma.masked_where(intersection_mask == 0, intersection_mask)  ,alpha=0.8)
+                # plt.title("PRE EXPAND")
+                # plt.show()
                 
                 intersection_mask = expand_mask(intersection_mask,20)
                 
@@ -274,10 +279,10 @@ def vis(model,im,depth_model):
                 if not np.any(tool_int) or not np.any(tissue_int):
                     continue
   
-                # plt.imshow(tool_int)
-                # plt.show()
-                # plt.imshow(tissue_int)
-                # plt.show()
+                plt.imshow(tool_int)
+                plt.show()
+                plt.imshow(tissue_int)
+                plt.show()
                 
                 depth_tool_int = depth_map[tool_int.astype(bool)]
                 depth_tissue_int = depth_map[tissue_int.astype(bool)]
@@ -286,21 +291,20 @@ def vis(model,im,depth_model):
                 med_tool = np.mean(depth_tool_int)
                 med_tissue = np.mean(depth_tissue_int)
                 
-                print(med_tool)
-                print(med_tissue)
+                # print(med_tool)
+                # print(med_tissue)
                 
                 if np.isnan(med_tool) or np.isnan(med_tissue):
                     continue
 
                 
-                tolerance = 0.06
+                tolerance = 0.1
     
                 
                 if np.abs(med_tool - med_tissue) <= tolerance:
                     pairs.append((tool_mask,tissue_mask))
-                
-        
-        
+                    
+                    
     if len(pairs) == 0:
         print("NO TTI FOUND")
     for pair in pairs:  
@@ -309,32 +313,116 @@ def vis(model,im,depth_model):
         ti = np.ma.masked_where(pair[1] == 0, pair[1])  
         plt.imshow(to,alpha=0.5, cmap='jet')
         plt.imshow(ti,alpha=0.5,cmap='Greens')
-        
-        # plt.gca().add_patch(rect)
-
-        plt.title(f" TTI ")
+        plt.title(" TTI ")
         plt.axis("off")
         plt.show()
+    return pairs
+                
+        
+        
         
     
+    
+def count_ttis(label_path):
+    with open(label_path, 'r', encoding='utf-8') as f:
+        ttis = 0
+        for line in f:
+            if not line:
+                continue
+            line = line.strip()
+            is_tti = int(line[0])
+            if is_tti == 1:
+                ttis += 1
+                
+    if ttis > 2:
+        print("More than 2 ttis")
+        return 2
+    return ttis
+        
+            
     
     
 def evaluate():
-    model = YOLO(CHECKPOINT)
-    img_fp = "../Full Dataset/val/video0194_frame104.jpg"
-    sd = torch.load(CHECKPOINT, map_location=DEVICE)
-    missing, unexpected = model.load_state_dict(sd, strict=False)
+    model = YOLO(CHECKPOINT) 
+    depth = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-small-hf")
     
-    depth = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Small-hf")
-    
-    images = os.listdir("../Full Dataset/val/")
+    images = os.listdir(IMAGES_FOR_EVAL_PATH)
     random.shuffle(images)
     
+    y_pred = []
+    y_true= []
+    i = 0
+    
+    n_no_pred = 0
+    n_wrong_masks = 0
+    n_diff_length = 0
+    tot_tti = 0
+    n_good_predictions = 0
+    
     for img_fp in images:
-        img_fp = os.path.join('../Full Dataset/val/',img_fp)
-        vis(model,img_fp,depth)
-
+        print(f'processing {i}/{len(images)}')
+        image_path = os.path.join(IMAGES_FOR_EVAL_PATH,img_fp)
+        binary_mask_path = os.path.join(BINARY_MASKS_PATH,img_fp.replace('.jpg','.png'))
+        labels_path = os.path.join(LABELS_TXT_PATH,img_fp.replace('.jpg','.txt'))
+        
+        msk = cv2.imread(binary_mask_path,cv2.IMREAD_GRAYSCALE)
+        
+        n_tti = count_ttis(labels_path)
+        pairs = vis(model,image_path,depth)
+        tot_tti += n_tti
+        
+        if n_tti > 0 and pairs is None:
+            y_true.append(1)
+            y_pred.append(0)
+            n_no_pred += 1
+            continue
+        elif n_tti == 0 and pairs is None:
+            y_true.append(1)
+            y_pred.append(1)
+            n_good_predictions+=1
+            continue
+        
+        # if n_tti != len(pairs):
+        #     y_true.append(1)
+        #     y_pred.append(0)
+        #     n_diff_length += 1
+        
+        for tool_mask , tissue_mask in pairs:
+            tool_int = np.logical_and(tool_mask,msk).sum()
+            mask_int = np.logical_and(tissue_mask,msk).sum()
+            
+            if tool_int > 0 or mask_int > 0:
+                y_true.append(1)
+                y_pred.append(1)
+                n_good_predictions+=1
+            else:
+                y_true.append(1)
+                y_pred.append(0)
+                n_wrong_masks += 1
+        i+=1
+        
+    print(f'Number different length: {n_diff_length}')
+    print(f'Number no preds: {n_no_pred}')
+    print(f'Number Wrong masks: {n_wrong_masks}')
+    print(f'Total TTIS: {tot_tti}')
+    print(f'Total good predictions: {n_good_predictions}')
+    
+    return y_pred,y_true
+            
 
 
 if __name__ == '__main__':
-    create_test()
+    # create_test()
+    y_pred,y_true = evaluate()
+    
+    zeros = 0
+    ones = 0
+    for i in y_pred:
+        if i == 0:
+            zeros += 1
+        else:
+            ones += 1
+    
+    print("Ones: ", ones)
+    print("Zeros: ", zeros)       
+    print("Accuracy: ", accuracy_score(y_true, y_pred))
